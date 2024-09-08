229d76cf58907359ff9f813d97b5a8be4cd3b12b
IgorPolotai <igor.polotai@gmail.com>
2024-09-08 15:48:42 -0400

    Finished the Simple HHTP Server Assignment

---
+++ src/textResponses.js
@@ -0,0 +1,28 @@
+const hello = 'Hello World!';
+
+const getTimeString = () => {
+  const d = new Date();
+  const dateString = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+  return dateString;
+};
+
+const getTime = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write(getTimeString());
+  response.end();
+};
+
+const getHello = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write(hello);
+  response.end();
+};
+
+// This exports these functions and variables by
+// just overwriting the entire empty module.exports object
+module.exports = {
+  hello,
+  getTimeString,
+  getHello,
+  getTime,
+};
